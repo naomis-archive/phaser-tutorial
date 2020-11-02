a884deeaf8e4071fa14b43d07bad291daaafbad3
nhcarrigan <nhcarrigan@gmail.com>
2020-11-02 13:24:44 -0800

    Pathing

--- public/script.js
+++ public/script.js
@@ -19,11 +19,11 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("sky", "../assets/sky.png");
-  this.load.image("ground", "../assets/platform.png");
-  this.load.image("star", "../assets/star.png");
-  this.load.image("bomb", "../assets/bomb.png");
-  this.load.spritesheet("dude", "../assets/dude.png", {
+  this.load.image("sky", "./assets/sky.png");
+  this.load.image("ground", "./assets/platform.png");
+  this.load.image("star", "./assets/star.png");
+  this.load.image("bomb", "./assets/bomb.png");
+  this.load.spritesheet("dude", "./assets/dude.png", {
     frameWidth: 32,
     frameHeight: 48,
   });
